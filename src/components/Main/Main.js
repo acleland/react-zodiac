fc325afdf7137a7338d257d2a1db24aaa5765e09
Andrea Cleland <andreaclelan@gmail.com>
2022-03-08 16:55:39 -0800

    added background image to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,11 @@ import './Main.css';
 import Sign from '../Sign/Sign';
 
 import { zodiac } from '../../data';
+import stars from '../../stars.jpg';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${stars})` }}>
       {zodiac.map((sign) => (
         <Sign key={sign.name} name={sign.name} dates={sign.dates} />
       ))}
